d1da3c80f0e6e5a24657de7f93b1230ffc61c474
Terje Berg-Hansen <terje@itfakultetet.no>
2017-09-27 19:51:43 +0200

    filer.pl og krig og fred

---
+++ filer.pl
@@ -0,0 +1,42 @@
+#! /usr/bin/perl
+
+my $filnavn = '/home/terje/Utvikling/perl/perl-grunnkurs-master/war-and-peace.txt';
+open my $kof, '<', $filnavn
+  or die "Kan ikke åpne < $filnavn: $!";
+
+my $i = 1;
+my %ord;
+
+while (my $line = <$kof>) {
+	chomp $line;
+	foreach my $str($line =~ /\w+/g) {
+		$ord{$str}++;
+	}
+	$i++;
+}
+
+my $utfilnavn_alfa = 'kof_alfabetisk.txt';
+my $utfilnavn_antall = 'kof_antall.txt';
+
+unlink $utfilnavn_alfa;
+unlink $utfilnavn_antall;
+
+open my $kofut_alfa, '>>', $utfilnavn_alfa
+  or die "Kan ikke lage eller skrive til < $utfilnavn_alfa: $!";
+
+foreach my $str (sort keys %ord) {
+	print $kofut_alfa "$str,$ord{$str}\n";
+}
+
+open my $kofut_antall, '>>', $utfilnavn_antall
+  or die "Kan ikke lage eller skrive til < $utfilnavn_alfa: $!";
+
+foreach my $str (sort { $ord{$a} <=> $ord{$b} or $a cmp $b } keys %ord) {
+	print $kofut_antall "$str,$ord{$str}\n";
+}
+
+
+print "Antall linjer i filen: $i \n";
+print "Antall ord i filen: ", $ord.length;
+
+close $kof or die "Kunne ikke lukke $filnavn: $!";
