5a47019874d961bc57a353d4d478890c596cfc3b
Terje Berg-Hansen <terje@itfakultetet.no>
2017-12-01 16:26:49 +0100

    Ny versjon

--- json.pl
+++ json.pl
@@ -1,6 +1,8 @@
 #! /usr/bin/perl
 use feature ':all';
 use JSON;
+# binmode STDOUT, ":utf8";
+# use utf8;
 
 my @liste = (2, 4, 6);
 push @liste, qw(1 2 3 4 5 6 7 8) ;
@@ -16,13 +18,34 @@ say "-----------------";
 say $json_text;
 say "-----------------";
 
-# lagfil();
+lagfil();
 
 sub lagfil {
-my $filnavn = 'terje.json';
+my $filnavn = 'data.json';
 
 open my $fil, '>', $filnavn
   or die "Kan ikke lagre til fil..$!";
 
 print $fil $json_text;
+}
+
+# -------------------------------
+# Les inn JSON-fil
+
+my $json;
+my $data;
+
+lesfil();
+# Print ut en variabel eller to
+say "Navnet er: ".$data->{'Navn'};
+say "Postnummeret er: ".$data->{'Adresse'}->{'Postnr'};
+say "Tall nummer 4 er: ".$data->{'Tall'}->[3];
+
+sub lesfil {
+   local $/; #Enable 'slurp' mode, som leser inn filen i ett "jafs", og ikke linje for linje...
+   open my $fh, "<", "data.json";
+   $json = <$fh>;
+   close $fh;
+
+   $data = decode_json($json);
 }
