e98ace9efcba588758cd0d6a8162fd41fbf7db8d
terjebh <terje@itfakultetet.no>
2017-12-02 19:30:40 +0100

    ny versjon

--- funksjoner.pl
+++ funksjoner.pl
@@ -1,15 +1,42 @@
 #! /usr/bin/perl
-
+use strict;
+use warnings;
+use feature ':all';
+use utf8;
 # Dette er en enkel funksjon som kaster en terning
 my $result = random_terningkast();
 
-print "Terningkast: $result\n";
+say "------------------------------------";
+say "Terningkast: $result";
+say "------------------------------------";
 
 sub random_terningkast {
 return 1 + int( rand(6) );
 }
 
-sub skrivut {
 
+# Funksjon som lager et html-oppsett
+
+my $ny_html = lag_html("Dette er tittelen","Dette er innholdet på siden","navy","white");
+say "------------------------------------";
+say "HTML:\n$ny_html";
+say "------------------------------------";
+
+# Skriv HTML til fil
+open my $fh, '>' , 'index.html';
+print $fh $ny_html;
+
+sub lag_html {
+
+my $html = <<"end"
+<html>
+<head>
+<title>@_[0]</title>
+</head>
+<body style="background: @_[2]; color: @_[3]">
+<p>@_[1]</p>
+</body>
+</html>
+end
 
 }
