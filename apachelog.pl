798dea52b99266107475a4a3fc6572f2d9d74b3d
terjebh <terje@itfakultetet.no>
2017-12-04 23:40:40 +0100

    ny

--- apachelog.pl
+++ apachelog.pl
@@ -1,27 +1,43 @@
 #! /usr/bin/perl
-
 use strict;
+use warnings;
 use feature ':all';
 
-my @host;
+my %hosts;
+my %angrep;
 
-open my $logfil, '<', '/var/log/apache2/access.log'
+open my $logfil, '<', 'access.log'
     or die "Kan ikke åpne: $!";
 
-
  while (my $line=<$logfil>) {
-    my ($host,$date,$url_with_method,$status,$size,$referrer,$agent) = $line =~
+   chomp $line;
+   # Lagre linjen i ulike variabler basert på et regulært uttrykk
+   my ($host,$date,$url_with_method,$status,$size,$referrer,$agent) = $line =~
           m/^(\S+) - - \[(\S+ [\-|\+]\d{4})\] "(\S+ \S+ [^"]+)" (\d{3}) (\d+|-) "(.*?)" "([^"]+)"$/;
-  
 
- next if $host =~ m#127.0.0.1#;
-	
- push @host, $host; 
-  
+   next if $host =~ m#127.0.0.1#;   # Ikke regn med egne hits, gå videre med next
 
+   $hosts{$host}++;  # øk med 1 hit
 
- foreach my $h (@host) {
-      print "$h\n";
+   if ($url_with_method=~m/%22|unhex|select/) {
+     $angrep{$host} .= $url_with_method."\n";
     }
- 
+
+ }
+
+#  skriv ut hits
+ foreach my $h (sort { $hosts{$a} <=> $hosts{$b} } keys %hosts) {
+      say "Host: $h - Antall hits: $hosts{$h}";
+    }
+
+say "\n--------------------------";
+
+#  skriv ut angrep
+for my $ang (keys %angrep) {
+
+say "Angrep fra $ang
+--------------------------
+$angrep{$ang}
+--------------------------";
+
 }
